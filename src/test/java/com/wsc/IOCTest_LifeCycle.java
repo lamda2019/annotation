f253bac1c1764f27725713d721500f10bf373ca5
e <569188901@qq.com>
2019-04-30 14:23:59 +0800

    @PostConstruct和@PreDestroy的使用

--- src/test/java/com/wsc/IOCTest_LifeCycle.java
+++ src/test/java/com/wsc/IOCTest_LifeCycle.java
@@ -40,4 +40,13 @@ public class IOCTest_LifeCycle {
         applicationContext.close();
     }
 
+    @Test
+    public void testPostConstructPreDestroy(){
+        //1、创建ioc容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
+        System.out.println("容器创建完成...");
+        //关闭容器
+        applicationContext.close();
+    }
+
 }
