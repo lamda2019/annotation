77b7e258e1c3ee7d92773d851af60f6acce7f934
e <569188901@qq.com>
2019-04-30 10:13:46 +0800

    注解方式扫描组件

---
+++ src/main/java/com/wsc/config/MainConfigIOC.java
@@ -0,0 +1,10 @@
+package com.wsc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value="com.wsc")
+public class MainConfigIOC {
+
+}
--- src/test/java/com/wsc/IOCTest.java
+++ src/test/java/com/wsc/IOCTest.java
@@ -1,8 +1,10 @@
 package com.wsc;
 
 
+import com.wsc.config.MainConfigIOC;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IOCTest {
@@ -19,6 +21,19 @@ public class IOCTest {
         for (String name : definitionNames) {
             System.out.println("xml方式扫描组件："+name);
 
+        }
+    }
+
+    /*注解方式
+     *
+     * 查看容器中有多少已经注册的bean
+     */
+    @Test
+    public void testRegisterBeanByConfiger(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigIOC.class);
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : definitionNames) {
+            System.out.println("注解方式扫描组件："+name);
         }
     }
 
