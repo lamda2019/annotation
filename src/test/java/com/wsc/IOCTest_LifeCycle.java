f880e90a40cc66f5d8c1d8991b510154e380072b
e <569188901@qq.com>
2019-04-30 14:26:59 +0800

    BeanPostProcessor后置处理器的使用

--- src/test/java/com/wsc/IOCTest_LifeCycle.java
+++ src/test/java/com/wsc/IOCTest_LifeCycle.java
@@ -49,4 +49,14 @@ public class IOCTest_LifeCycle {
         applicationContext.close();
     }
 
+
+    @Test
+    public void testBeanPostProcessor(){
+        //1、创建ioc容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
+        System.out.println("容器创建完成...");
+        //关闭容器
+        applicationContext.close();
+    }
+
 }
