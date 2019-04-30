8059d2f0930d27fa7437522a9d9880ce397646a0
e <569188901@qq.com>
2019-04-30 10:20:51 +0800

    ComponentScans代替ComponentScan可以指定多个扫描规则

--- src/main/java/com/wsc/config/MainConfigIOC.java
+++ src/main/java/com/wsc/config/MainConfigIOC.java
@@ -1,14 +1,19 @@
 package com.wsc.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(value="com.wsc",includeFilters= {
-        @ComponentScan.Filter(type= FilterType.ANNOTATION,classes={Controller.class})
-})
+@ComponentScans(
+        value = {
+                @ComponentScan(value="com.wsc",includeFilters = {
+                        @ComponentScan.Filter(type=FilterType.ANNOTATION,classes={Controller.class}),
+                },useDefaultFilters = false)
+        }
+)
 
 public class MainConfigIOC {
 
