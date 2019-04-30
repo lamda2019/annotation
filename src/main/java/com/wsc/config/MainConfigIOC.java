2ff2e1aaa18ae56297d492c22041f03e2040ebcf
e <569188901@qq.com>
2019-04-30 10:22:54 +0800

    ASSIGNABLE_TYPE的使用

--- src/main/java/com/wsc/config/MainConfigIOC.java
+++ src/main/java/com/wsc/config/MainConfigIOC.java
@@ -1,5 +1,6 @@
 package com.wsc.config;
 
+import com.wsc.servicer.PersonServicer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
         value = {
                 @ComponentScan(value="com.wsc",includeFilters = {
                         @ComponentScan.Filter(type=FilterType.ANNOTATION,classes={Controller.class}),
+                        @ComponentScan.Filter(type=FilterType.ASSIGNABLE_TYPE,classes={PersonServicer.class}),
                 },useDefaultFilters = false)
         }
 )
