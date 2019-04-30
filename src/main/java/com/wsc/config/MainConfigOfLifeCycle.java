ec17d2c818ff99e4c513ec7ed120762731d6a89a
e <569188901@qq.com>
2019-04-30 11:10:03 +0800

    InitializingBean和DisposableBean的使用

--- src/main/java/com/wsc/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/wsc/config/MainConfigOfLifeCycle.java
@@ -1,13 +1,14 @@
 package com.wsc.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.wsc.bean.Car;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
-
+@ComponentScan("com.wsc.bean")
 public class MainConfigOfLifeCycle {
 
     @Scope("prototype")
--- src/test/java/com/wsc/IOCTest_LifeCycle.java
+++ src/test/java/com/wsc/IOCTest_LifeCycle.java
@@ -29,4 +29,15 @@ public class IOCTest_LifeCycle {
         applicationContext.close();
     }
 
+
+    @Test
+    public void testInitializingBeanDisposableBean(){
+        //1、创建ioc容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
+        System.out.println("容器创建完成...");
+
+        //关闭容器
+        applicationContext.close();
+    }
+
 }
