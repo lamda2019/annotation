d9543baedb7b4783b0c787cbc52ebfa83318350b
e <569188901@qq.com>
2019-04-30 11:04:16 +0800

    多实例声明周期

--- src/test/java/com/wsc/IOCTest_LifeCycle.java
+++ src/test/java/com/wsc/IOCTest_LifeCycle.java
@@ -17,4 +17,16 @@ public class IOCTest_LifeCycle {
         applicationContext.close();
     }
 
+    @Test
+    public void testLifeCyclePrototype(){
+        //1、创建ioc容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfLifeCycle.class);
+        System.out.println("容器创建完成...");
+
+        applicationContext.getBean("car");//调用该bean
+        System.out.println("多实例声明周期中容器不会调用销毁方法");
+        //关闭容器
+        applicationContext.close();
+    }
+
 }
