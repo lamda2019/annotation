3907690b253d2a92e1ed91635d5d3e170f4993df
e <569188901@qq.com>
2019-04-30 11:02:36 +0800

    多实例声明周期

--- src/main/java/com/wsc/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/wsc/config/MainConfigOfLifeCycle.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.wsc.bean.Car;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+
 public class MainConfigOfLifeCycle {
 
+    @Scope("prototype")
     @Bean(initMethod="init",destroyMethod="detory")
     public Car car(){
         return new Car();
